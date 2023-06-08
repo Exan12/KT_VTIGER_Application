001b5d9a6b1d8e996316e86227aaf6221a2251a0
Lenovo <Lenovo@DESKTOP-GE7D7VL>
2023-06-08 18:42:56 +0530

    Organization Module Commit

--- src/test/java/Practice/HomeTest.java
+++ src/test/java/Practice/HomeTest.java
@@ -1,10 +1,37 @@
 package Practice;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class HomeTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AWTException {
+		
 		
 		System.out.println("Entered into v-Tiger Home Page");
+		Robot r = new Robot();
+		r.keyPress(KeyEvent.VK_TAB);
+		r.keyRelease(KeyEvent.VK_TAB);
+		System.out.println("Successfully Pressed and released TAB Key");
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver =  new ChromeDriver(co);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.google.com/");
+		r.keyPress(KeyEvent.VK_F5);
+		r.keyRelease(KeyEvent.VK_F5);
+		driver.navigate().refresh();
+		System.out.println("Successfully refreshed the Page");
 	}
 
 }
---
+++ src/test/java/Practice/OrganizationPage.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class OrganizationPage {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Organization Page Created");
+	}
+
+}
